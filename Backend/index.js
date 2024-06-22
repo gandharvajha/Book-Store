78b54143bad3c46dce58091df04ebf1535f63aa6
gandharvajha <gandharva1371998@gmail.com>
2024-06-22 15:27:43 +0530

    updated code

--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,33 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import bookRoute from './route/book.route.js';
+import userRoute from './route/user.route.js';
 
 const app = express();
+
 dotenv.config();
 
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 4000;
-const URI = process.env.mongoDBURI;
+const URI = process.env.MONGO_URI;
 
-// Connect to MongoDB
 mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(error => console.log("Error connecting to MongoDB:", error));
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.log('Error:', error);
+});
 
-// Other server setup and routes
+app.use('/book', bookRoute);
+app.use('/user', userRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
